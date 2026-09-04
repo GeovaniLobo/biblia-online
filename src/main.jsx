c20c9dab6109b4e19b9a2a270f6b2cd1c04c2200
GeovaniLobo <geovanilobo98@gmail.com>
2026-09-04 10:15:10 -0300

    Configura favicon importado de src/assets e atualiza titulo para Luz do Mundo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Importe o seu favicon de dentro de assets (ajuste o nome do arquivo se for diferente, ex: favicon.png ou logo.png)
+import meuFavicon from './assets/favicon.png'
+
+// Define dinamicamente o favicon na aba do navegador
+const linkRelIcon = document.querySelector("link[rel*='icon']") || document.createElement('link');
+linkRelIcon.type = 'image/png';
+linkRelIcon.rel = 'icon';
+linkRelIcon.href = meuFavicon;
+document.getElementsByTagName('head')[0].appendChild(linkRelIcon);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
